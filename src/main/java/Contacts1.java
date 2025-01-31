208823a80f0936a61619b846d5bbe7d85114c959
rchlai <22b6034@ubd.edu.bn>
2025-01-31 16:04:32 +0800

    Proofread some texts

--- src/main/java/Contacts1.java
+++ src/main/java/Contacts1.java
@@ -26,9 +26,9 @@ public class Contacts1 {
     /*
      * NOTE : ==================================================================
      * These messages shown to the user are defined in one place for convenient
-     * editing and proof reading. Such messages are considered part of the UI
+     * editing and proofreading. Such messages are considered part of the UI
      * and may be subjected to review by UI experts or technical writers. Note
-     * that Some of the strings below include '%1$s' etc to mark the locations
+     * that Some of the strings below include '%1$s' etc. to mark the locations
      * at which java String.format(...) method can insert values.
      * =========================================================================
      */
